014a07845bdb4eddc1cb7c3a6c1588e9903a7818
ziyan1215 <xuziyanmiss@gmail.com>
2019-01-02 23:41:57 +0800

    自动提交：2019-01-02_23.41.57

--- push.bat
+++ push.bat
@@ -1,4 +1,11 @@
+set dd=%date:~8,2%
+set mm=%date:~5,2%
+set yy=%date:~0,4%
+set Tss=%TIME:~6,2%
+set Tmm=%TIME:~3,2%
+set Thh=%TIME:~0,2%
+set Thh=%Thh: =0%
 git add .
-git commit -m "自动提交："%date:~0,4%年%date:~5,2%月%date:~8,2%日
+git commit -m "自动提交："%yy%-%mm%-%dd%_%Thh%.%Tmm%.%Tss%
 git push origin master
 pause
