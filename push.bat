f639cb364bb6b478d6e3d77acd65c66f7f52f2bb
ziyan1215 <xuziyanmiss@gmail.com>
2019-01-02 23:43:07 +0800

    自动提交：2019-01-02-23.43.07

--- push.bat
+++ push.bat
@@ -6,6 +6,6 @@ set Tmm=%TIME:~3,2%
 set Thh=%TIME:~0,2%
 set Thh=%Thh: =0%
 git add .
-git commit -m "自动提交："%yy%-%mm%-%dd%_%Thh%.%Tmm%.%Tss%
-git push origin master
+git commit -m "自动提交："%yy%-%mm%-%dd%-%Thh%.%Tmm%.%Tss%
+
 pause
