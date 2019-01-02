fdca4122a672c8f7e941fd087f7258a11a4a8009
ziyan1215 <xuziyanmiss@gmail.com>
2019-01-02 23:39:36 +0800

    自动提交：2019年01月02日

--- push.bat
+++ push.bat
@@ -1,6 +1,4 @@
-@echo off
-@title bat 交互执行git命令
 git add .
-git commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日
+git commit -m "自动提交："%date:~0,4%年%date:~5,2%月%date:~8,2%日
 git push origin master
 pause
