156d6b474d4e2273f25528ef3957cb5be42749bc
xuziyan <xuziyanmiss@gmail.com>
2019-01-21 10:25:48 +0800

    自动提交：2019-01-21_10：25：48

--- bin/Timing.sh
+++ bin/Timing.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-echo "字段脚本开始启动..."
+echo "begin..."
 
 #oracle info
 dateTime=`date +%Y_%m_%d`
 days=7
-
-echo "现在开始进行数据更新..."
+echo $dateTime
+echo "update git..."
 git pull origin master
 echo "auto update json file:"$dateTime > log.txt
 echo. > log.txt
-git add .
-git commit -m "auto update json file"
-git push origin master 
-echo "数据备份结束..."
-echo "现在开始进行压缩备份数据..."
-echo "删除备份数据结束..."
-echo "备份执行完毕..."
+#git add .
+#git commit -m "auto update json file"
+#git push origin master 
