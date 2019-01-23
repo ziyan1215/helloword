90637d1246546781910c5e60da532e40ef60f029
git <xuziyanmiss@gmail.com>
2019-01-23 10:07:38 +0800

    auto update json file

--- bin/Timing.sh
+++ bin/Timing.sh
@@ -9,7 +9,7 @@ echo "-----------------update from github..."
 git pull origin master
 #Ã»ÓÃÁË£ºcp /opt/blog/static/data/shanbayToday.json /opt/blog/static/data/shanbayToday$dateTime.json
 echo "-----------------get json file"
-
+curl -L  "http://rest.shanbay.com/api/v2/quote/quotes/today/?_=1546075870467" >>/opt/blog/bin/getJson/$dateTime.json
 cd /opt/blog/bin
 echo "-----------------auto update json file:"$dateTime >> log.txt
 
