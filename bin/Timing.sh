2c7b12b12656ac3cdfd47814fb230e00924d930f
git <xuziyanmiss@gmail.com>
2019-01-23 09:55:19 +0800

    auto update json file

--- bin/Timing.sh
+++ bin/Timing.sh
@@ -3,16 +3,17 @@ dateTime=`date +%Y-%m-%d,%H:%m:%s`
 days=7
 echo $dateTime
 # bigin
-echo "go index to update git"
+echo "-----------------go index to update git"
 cd  /opt/blog
-echo "update from github..."
+echo "-----------------update from github..."
 git pull origin master
 #没用了：cp /opt/blog/static/data/shanbayToday.json /opt/blog/static/data/shanbayToday$dateTime.json
 cd /opt/blog/bin
-echo "auto update json file:"$dateTime >> log.txt
+echo "-----------------auto update json file:"$dateTime >> log.txt
 
-echo "去主目录sumit to github..."
+echo "-----------------go index sumit to github..."
 cd  /opt/blog
 git add .
 git commit -m "auto update json file"
 git push origin master 
+echo "----------------finish"
