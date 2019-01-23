8551444b6326471017447ed86370c670275c1326
git <xuziyanmiss@gmail.com>
2019-01-23 10:01:13 +0800

    auto update json file

--- bin/Timing.sh
+++ bin/Timing.sh
@@ -9,13 +9,13 @@ echo "-----------------update from github..."
 git pull origin master
 #Ã»ÓÃÁË£ºcp /opt/blog/static/data/shanbayToday.json /opt/blog/static/data/shanbayToday$dateTime.json
 echo "-----------------get json file"
-
+curl -L  "http://rest.shanbay.com/api/v2/quote/quotes/today/?_=1546075870467" >>/opt/blog/bin/getJson/$dateTime.json
 cd /opt/blog/bin
 echo "-----------------auto update json file:"$dateTime >> log.txt
 
 echo "-----------------go index sumit to github..."
 cd  /opt/blog
-git add .
+git add -A
 git commit -m "auto update json file"
 git push origin master 
 echo "----------------finish"
